2553b7214156669785ae4173e610e56cc621b144
Deepanshu Rana <deepanshurana4500@gmail.com>
2019-10-19 23:01:35 +0530

    ClassBased Views

---
+++ basicApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from basicApp.models import School, Student
+# Create your views here.
+
+
+class indexView(TemplateView):
+
+    template_name = 'basicApp/home.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'Basic Injection'
+        return context
+
+
+class schoolListView(ListView):
+    model = School
+    context_object_name = 'schools'
+    template_name = 'basicApp/schoolList.html'
+
+
+class schoolDetailView(DetailView):
+    context_object_name = 'schoolDetails'
+    model = School
+    template_name = 'basicApp/schoolDetail.html'
